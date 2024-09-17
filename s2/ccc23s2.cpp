b0b29467fed4e39176eb413858eaa339c1d34200
unknown <sharon@basovich.com>
2024-09-16 22:24:41 -0400

    part marks on s2 but timeout

--- s2/ccc23s2.cpp
+++ s2/ccc23s2.cpp
@@ -12,25 +12,31 @@ int main()
     }
 
     // runs for each length of crop
-    for (int i = 0; i < N; i++)
+    for (int i = 1; i < N + 1; i++)
     {
-        int combinations = N - i;
+        int combinations = N - i + 1;
         int calc[combinations][i];
         // runs for amount each crop occurs
         for (int j = 0; j < combinations; j++)
         {
             // gets all number in crop
-            for (int k = 0; k < (i + 1); k++)
+            for (int k = 0; k < i; k++)
             {
                 calc[j][k] = arr[j + k];
+                //cout << arr[j+k];
             }
         }
+
+        int symmetry[combinations];
         for (int j = 0; j < combinations; j++)
         {
+            symmetry[j] = 0;
             for (int k = 0; k < (i + 1) / 2; k++)
             {
-                cout << abs(calc[j][k] + calc[j][i-k]) << endl;
+                symmetry[j] += abs(calc[j][k] - calc[j][i-1-k]);
             }
         }
+        sort(symmetry, symmetry + combinations);
+        cout << symmetry[0] << " ";
     }
 }
