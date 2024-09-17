6d304258237cf76c110787af5f725dd9d263a5ef
unknown <sharon@basovich.com>
2024-09-17 17:23:24 -0400

    part marks for s1

---
+++ misc/data.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a; int b; int c; int d;
+    for (int i = 0; i < 38; i++)
+    {
+        cin >> a >> b >> c >> d;
+    }
+}
---
+++ s1/ccc22s1.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+1 2 3 4 5 6 7 8 9 10
+*/
+
+int main()
+{
+    int N; cin >> N;
+    int groups = N / 20;
+    //map<int, int> solution = {{0, 0}, {1, 0}, {2, 0}, {3, 0}, {4, 1}, {5,1}, {6,0}, {7,0}, {8, 1}, {9,1}, {10,1}};
+    groups = (int)pow(2.0, (double)groups);
+    //cout << solution.at(N);
+}
